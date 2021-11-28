bf2ba0700f834fe81fe5ca993ee553e5e8725a19
Theodora Chu <theodorachu@gmail.com>
2021-11-27 19:30:00 -0500

    toggle buttons + footer added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
 import './App.css';
+import React, {useState} from 'react';
+import ToggleButton from '@mui/material/ToggleButton';
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
 
 function App() {
+  const [body, setBody] = useState("");
+  const handleBody = (event) => {
+    setBody(event.target.value);
+  }
+  
   return (
     <div className="App">
       <header>
+        <ToggleButtonGroup
+          value={body}
+          exclusive
+          onChange={handleBody}>
+          <ToggleButton value="upper">Upper Body</ToggleButton>
+          <ToggleButton value="lower">Lower Body</ToggleButton>
+        </ToggleButtonGroup>
       </header>
       <footer>
-        <div>Icons made by <a href="https://www.flaticon.com/authors/ddara" title="dDara">dDara</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+        <p>made with luv © {(new Date().getFullYear())}</p>
+        <p>favicon made by <a href="https://www.flaticon.com/authors/ddara" title="dDara">dDara</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></p>
       </footer>
     </div>
   );
+
 }
 
 export default App;
