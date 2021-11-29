d02299404a76b46b5bd616e12c90a9b8e62cd458
Theodora Chu <theodorachu@gmail.com>
2021-11-29 01:43:44 -0500

    added reps per side

--- src/components/Workout.jsx
+++ src/components/Workout.jsx
@@ -1,60 +1,60 @@
 import React from 'react';
 
 function Workout(props) {
-  const Exercise = (name, reps, canUseEquipment, canNoEquipment) => {return {name: name, reps: reps, canUseEquipment: canUseEquipment, canNoEquipment: canNoEquipment}};
+  const Exercise = (name, reps, perSide, canUseEquipment, canNoEquipment) => {return {name: name, reps: reps, perSide: perSide, canUseEquipment: canUseEquipment, canNoEquipment: canNoEquipment}};
   const arms = [
-    Exercise("Bent Over Rows", 12, true, false),
-    Exercise("Overhead Press", 12, true, false),
-    Exercise("Overhead Incline Press", 12, true, false),
-    Exercise("Bench", 12, true, false),
-    Exercise("Lat Pulldown", 8, true, false),
-    Exercise("Low to High", 12, true, false),
-    Exercise("Squat & Press", 10, true, false),
-    Exercise("Bicep Curl", 10, true, false),
-    Exercise("Shoulder Shrug", 10, true, false),
-    Exercise("Chest Fly", 10, true, false),
-    Exercise("Forwards Arm Raise", 10, true, false),
-    Exercise("Outwards Arm Raise", 10, true, false),
-    Exercise("Pushups", 8, false, true),
-    Exercise("Tricep Dips", 10, false, true),
-    Exercise("Burpees", 10, false, true),
-    Exercise("Spider Pushups", 8, false, true),
-    Exercise("Arm Circles", 30, false, true),
-    Exercise("Plank Walk", 6, false, true)
+    Exercise("Bent Over Rows", 12, false, true, false),
+    Exercise("Overhead Press", 12, false, true, false),
+    Exercise("Overhead Incline Press", 12, false, true, false),
+    Exercise("Bench", 12, false, true, false),
+    Exercise("Lat Pulldown", 8, false, true, false),
+    Exercise("Low to High", 6, true, true, false),
+    Exercise("Squat & Press", 10, false, true, false),
+    Exercise("Bicep Curl", 10, false, true, false),
+    Exercise("Shoulder Shrug", 10, false, true, false),
+    Exercise("Chest Fly", 10, false, true, false),
+    Exercise("Forwards Arm Raise", 10, false, true, false),
+    Exercise("Outwards Arm Raise", 10, false, true, false),
+    Exercise("Pushups", 8, false, false, true),
+    Exercise("Tricep Dips", 10, false, false, true),
+    Exercise("Burpees", 10, false, false, true),
+    Exercise("Spider Pushups", 4, true, false, true),
+    Exercise("Arm Circles", 30, false, false, true),
+    Exercise("Plank Walk", 6, false, false, true)
   ];
 
   const abs = [
-    Exercise("Russian Twists", 20, true, false),
-    Exercise("Plank Rows", 12, true, false),
-    Exercise("Bent Leg Jackknifes", 10, true, true),
-    Exercise("Plank Dumbbell Pull Through", 10, true, false),
-    Exercise("Leg Up Crunch Pulse", 20, true, true),
-    Exercise("Knees to Chest with Dumbbell Between Legs", 8, true, false),
-    Exercise("Vertical Scissor Kicks", 16, true, true),
-    Exercise("Horizontal Scissor Kicks", 16, false, true),
-    Exercise("Leg Raises with Hip Lift", 10, false, true),
-    Exercise("Side Planks", 10, true, true),
-    Exercise("Mountain Climbers", 40, false, true),
-    Exercise("Straight Leg Jackknifes", 8, true, true),
-    Exercise("Bicycle Crunches", 20, true, true)
+    Exercise("Russian Twists", 10, true, true, false),
+    Exercise("Plank Rows", 6, true, true, false),
+    Exercise("Bent Leg Jackknifes", 10, false, true, true),
+    Exercise("Plank Dumbbell Pull Through", 6, true, true, false),
+    Exercise("Leg Up Crunch Pulse", 20, false, true, true),
+    Exercise("Knees to Chest with Dumbbell Between Legs", 8, false, true, false),
+    Exercise("Vertical Scissor Kicks", 16, false, true, true),
+    Exercise("Horizontal Scissor Kicks", 16, false, false, true),
+    Exercise("Leg Raises with Hip Lift", 10, false, false, true),
+    Exercise("Side Planks", 10, true, true, true),
+    Exercise("Mountain Climbers", 40, false, false, true),
+    Exercise("Straight Leg Jackknifes", 8, false, true, true),
+    Exercise("Bicycle Crunches", 10, true, true, true)
   ];
 
   const lower = [
-    Exercise("Squat", 12, true, true),
-    Exercise("Reverse Lunge", 12, true, true),
-    Exercise("Side Lunge", 12, true, true),
-    Exercise("Curtsy Lunge", 12, true, true),
-    Exercise("Jump Squat", 10, false, true),
-    Exercise("Weighted Step Ups", 12, true, false),
-    Exercise("Single Leg Deadlift", 16, true, false),
-    Exercise("Hip Thrust", 10, true, false),
-    Exercise("Kettlebell Swing", 10, true, false),
-    Exercise("Clamshell", 20, false, true),
-    Exercise("Calf Raises", 16, true, true),
-    Exercise("Leg Press", 15, true, false),
-    Exercise("Leg Extension", 15, true, false),
-    Exercise("Crab Walk", 8, true, true),
-    Exercise("Laying Down Side Leg Raise", 20, false, true)
+    Exercise("Squat", 12, false, true, true),
+    Exercise("Reverse Lunge", 6, true, true, true),
+    Exercise("Side Lunge", 6, true, true, true),
+    Exercise("Curtsy Lunge", 6, true, true, true),
+    Exercise("Jump Squat", 10, false, false, true),
+    Exercise("Weighted Step Ups", 6, true, true, false),
+    Exercise("Single Leg Deadlift", 8, true, true, false),
+    Exercise("Hip Thrust", 10, false, true, false),
+    Exercise("Kettlebell Swing", 10, false, true, false),
+    Exercise("Clamshell", 10, true, false, true),
+    Exercise("Calf Raises", 16, false, true, true),
+    Exercise("Leg Press", 15, false, true, false),
+    Exercise("Leg Extension", 15, false, true, false),
+    Exercise("Crab Walk", 4, true, true, true),
+    Exercise("Laying Down Side Leg Raise", 10, true, false, true)
   ]
 
   if(props.type["Focus Area"].length === 0) return null;
@@ -97,7 +97,7 @@ function Workout(props) {
                   return(
                     <tr key={e.name}>
                       <td style={{textAlign:"left"}}>{e.name}</td>
-                      <td style={{textAlign:"right", whiteSpace:"nowrap"}}>{e.reps} Reps</td>
+                      <td style={{textAlign:"right", whiteSpace:"nowrap"}}>{e.reps} {(e.perSide) ? "Reps / Side" : "Reps"}</td>
                     </tr>
                   );
                 })}
@@ -113,7 +113,7 @@ function Workout(props) {
             return(
               <tr key={e.name}>
                 <td style={{textAlign:"left"}}>{e.name}</td>
-                <td style={{textAlign:"right", whiteSpace:"nowrap"}}>{e.reps} Reps</td>
+                <td style={{textAlign:"right", whiteSpace:"nowrap"}}>{e.reps} {(e.perSide) ? "Reps / Side" : "Reps"}</td>
               </tr>
             );
           })}
